b50383dfa19b1febaa7698976cf2d59e786018f9
Jacobo Tapia <jacob.et.cetera@gmail.com>
2019-05-02 22:30:21 -0500

    Fix typo

--- app/Http/Controllers/Api/V1/BaseController.php
+++ app/Http/Controllers/Api/V1/BaseController.php
@@ -7,7 +7,7 @@ use Illuminate\Routing\Controller;
  * @OA\Info(
  *      version="1",
  *      title="Containers",
- *      description="COntainers it's a project developed by @Jatapiaro",
+ *      description="Containers it's a project developed by @Jatapiaro",
  *      @OA\Contact(
  *          email="jatapiaro@gmail.com.com"
  *      ),
