671e207b577f5fed6edb18ca705503836d970147
Jacobo Tapia <jacob.et.cetera@gmail.com>
2019-05-02 22:28:12 -0500

    Añade documentación con swagger

---
+++ app/Http/Controllers/Api/V1/BaseController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers\Api\V1;
+
+use Illuminate\Routing\Controller;
+
+/**
+ * @OA\Info(
+ *      version="1",
+ *      title="COntainers",
+ *      description="COntainers it's a project developed by @Jatapiaro",
+ *      @OA\Contact(
+ *          email="jatapiaro@gmail.com.com"
+ *      ),
+ * )
+ */
+class BaseController extends Controller {
+
+}
