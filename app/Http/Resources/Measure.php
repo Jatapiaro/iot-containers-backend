c69961805671e7c4dc7fba16fe28d1fe9893c002
Jacobo Tapia <jacob.et.cetera@gmail.com>
2019-08-15 10:22:46 -0500

    Añade formato de fechas

--- app/Http/Resources/Measure.php
+++ app/Http/Resources/Measure.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class Measure extends JsonResource
 {
@@ -14,6 +15,14 @@ class Measure extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $data = [];
+        $data['id'] = $this->id;
+        $data['height'] = $this->height;
+        $data['volume'] = $this->volume;
+        $data['container_id'] = $this->container_id;
+        $createdAt = new Carbon($this->created_at);
+        $data['created_at'] = $createdAt->toDateTimeString();
+        $data['updated_at'] = $this->updated_at;
+        return $data;
     }
 }
